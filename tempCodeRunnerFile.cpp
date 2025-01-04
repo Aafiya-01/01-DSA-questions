3e4b897db568cea2034b5b6f8bfa21a980185b6a
Aafiya Choudhary <aafiyachoudhary0805@gmail.com>
2025-01-04 22:18:47 +0530

    full code

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,10 +1,4 @@
-    int n = 7;
-    int arr[7]={3, -4, 5, 4, -1, 7, -8};
-    int max_sum=INT_MIN;
-    for(int srt=0; srt<n; srt++){
-            int curr_sum=0;
-        for(int end=srt; end<n; end++){
-                  curr_sum += arr[end];
-                  max_sum= max(curr_sum, max_sum);
-        }    
-    }
+if( n < 0 ){
+    //     x = 1.0/x;
+    //     // bf = -bf;
+    // }
