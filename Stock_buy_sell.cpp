c5a4f3c4166b5262c444268d3a390ee2e9190016
Aafiya Choudhary <aafiyachoudhary0805@gmail.com>
2025-01-06 20:34:19 +0530

    sell and buy stock

---
+++ Stock_buy_sell.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+double buy_sell(vector<int>& prices){
+      
+}
+
+int main(){
+   vector<int> prices=[7,1,5,3,6,4];
+   res= buy_sell();
+   cout<< "result is: "<<res;
+   return 0;
+ }
