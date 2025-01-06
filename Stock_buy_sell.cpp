ddc4d97132576722345b56188ac8a558f7dfeb7d
Aafiya Choudhary <aafiyachoudhary0805@gmail.com>
2025-01-06 20:58:55 +0530

    solution done

--- Stock_buy_sell.cpp
+++ Stock_buy_sell.cpp
@@ -4,13 +4,22 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-double buy_sell(vector<int>& prices){
-      
-}
+int buy_sell(vector<int>& prices){
+      int mp = 0, best_buy = prices[0];
+      int n = prices.size();
+
+      for(int i=1; i<n; i++){
+        if(prices[i]> best_buy){
+          mp = max(mp, prices[i]- best_buy);
+        }
+          best_buy = min(best_buy, prices[i]);
+      }
+    return mp;  
+};
 
 int main(){
-   vector<int> prices=[7,1,5,3,6,4];
-   res= buy_sell();
+   vector<int> prices={7,1,5,3,6,4};
+   int res= buy_sell(prices);
    cout<< "result is: "<<res;
    return 0;
  }
