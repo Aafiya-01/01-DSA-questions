69a12082de4f383503f8e63fff124e4e2d48de71
Aafiya Choudhary <aafiyachoudhary0805@gmail.com>
2025-01-24 20:16:54 +0530

    diagonalSum optmised code done!!

--- DiagonslSum.cpp
+++ DiagonslSum.cpp
@@ -1,24 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int MaxColSum(int mat[][3], int rows, int col){
-    int max_sum=0;
-        for(int i=0; i<col; i++){
-            int sum = 0;
-            for(int j=0; j<rows; j++){
-                sum += mat[j][i];
-            }
-           max_sum= max(max_sum, sum); 
+int DiagonalSum(int mat[][4], int n){
+    int sum = 0;
+
+    //time complexity cuz of two loops = O(n*n)
+
+    // for(int i= 0 ; i<n; i++){
+    //     for(int j =0; j<n; j++){
+    //         if(i == j ){
+    //             sum += mat[i][j];
+    //         }else if(j == n-1-i){
+    //             sum += mat[i][j];
+    //         }
+    //     }
+    // }
+    // return sum;
+
+    //optimised code trying for = O(n)
+
+    for(int i =0; i< n; i++){
+        sum += mat[i][i];
+    //common value add na ho
+        if(i != n-i-1 ){
+            sum+= mat[i][n-i-1];
         }
-    return max_sum;
+    }
+    return sum;
 }
 
 int main(){
-    int mat[][3]={{1,2,3},{4,5,6},{7,8,9}};
-    int rows = 3;
-    int col = 3;
+    int mat[][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12}, {13,14,15,16}};
+    int n= 4;
 
-    int result= MaxColSum(mat, rows, col);
+    int result= DiagonalSum(mat, n);
     cout<< result <<endl;
     return 0;
 }
